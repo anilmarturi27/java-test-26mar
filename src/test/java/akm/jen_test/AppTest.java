925625871e432437e55fdeb8e94a12edd274e24c
anilmarturi27 <82029438+anilmarturi27@users.noreply.github.com>
2026-03-26 23:54:01 +0530

    added 5 tests

--- src/test/java/akm/jen_test/AppTest.java
+++ src/test/java/akm/jen_test/AppTest.java
@@ -7,6 +7,27 @@ public class AppTest {
 	@Test
 	public void test1() {
 		System.out.println("test 1 anil marturi");
+	}
+	
+	@Test
+	public void test2() {
 		System.out.println("test 2 anil marturi1");
 	}
+	
+	@Test
+	public void test3() {
+		System.out.println("test 3 anil marturi");
+	}
+	
+	@Test
+	public void test4() {
+		System.out.println("test 4 anil marturi");
+
+	}
+	
+	@Test
+	public void test5() {
+		System.out.println("test 5 anil marturi");
+	}
+	
 }
