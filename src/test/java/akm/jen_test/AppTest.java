94f727096bf3996c49b03a5601a82de4bc966327
anilmarturi27 <82029438+anilmarturi27@users.noreply.github.com>
2026-03-26 23:37:24 +0530

    second commit

--- src/test/java/akm/jen_test/AppTest.java
+++ src/test/java/akm/jen_test/AppTest.java
@@ -6,6 +6,7 @@ public class AppTest {
 	
 	@Test
 	public void test1() {
-		System.out.println("test1");
+		System.out.println("test 1 anil marturi");
+		System.out.println("test 2 anil marturi");
 	}
 }
